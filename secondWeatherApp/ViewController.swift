fd52906e495ba8b95c9c2a1c22ff6a54d6cba9fd
Vladislav Voloshenko <vladislawoloshenko@gmail.com>
2020-02-27 23:25:03 +0300

    Initial Commit

---
+++ secondWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  secondWeatherApp
+//
+//  Created by Влад Волошенко on 2/27/20.
+//  Copyright © 2020 Влад Волошенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
